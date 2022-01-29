e21e5f58a8a1340382008a6234a6f1322531a4c7
Ernesto <isaaccura.ernesto@gmail.com>
2022-01-29 21:43:31 +1100

    Updated RWB Features

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>I'm the footer</div>;
+  return (
+    <footer id='footer'>
+      <div id='copyright-notice'>&copy; 2022 ernestoIsaaccura</div>
+      <div id='website-author'>
+        Website by
+        <a
+          href='https://www.linkedin.com/in/eisaaccura/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Ernesto Isaaccura
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
